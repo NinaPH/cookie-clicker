cf806e9c2c2d3d980d0477c54f112bb64b18c1b8
Nina Plejdrup <ninaplejdrup@hotmail.com>
2025-08-29 21:24:29 +0200

    cursor functionality

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const cookie = document.querySelector(".cookie");
 const numContainer = document.querySelector(".num-cookies-container");
 const perSecContainer = document.querySelector(".per-second-container");
 
+const cursorBtn = document.querySelector(".cursor-btn");
+let perSec = 0;
+
+setInterval(() => {
+  cookies += perSec / 10;
+  numContainer.textContent = Math.floor(cookies);
+}, 100);
+
 const clickAudio = document.getElementById("clickAudio");
 
 let cookies = 0;
@@ -18,5 +26,16 @@ cookie.addEventListener("click", () => {
   cookie.classList.add("start-bouncing");
 
   cookies++;
-  numContainer.textContent = cookies;
+  numContainer.textContent = Math.floor(cookies);
+});
+
+cursorBtn.addEventListener("click", () => {
+  if (cookies >= 10) {
+    cookies -= 10;
+    numContainer.textContent = Math.floor(cookies);
+
+    perSec += 0.1;
+    perSec = Math.round(perSec * 10) / 10;
+    perSecContainer.textContent = perSec;
+  }
 });
