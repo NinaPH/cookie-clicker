f577d406240ee292cd4e782ef0a365db717d540a
Nina Plejdrup <ninaplejdrup@hotmail.com>
2025-08-29 22:23:31 +0200

    coded upgrades

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const cookie = document.querySelector(".cookie");
+const clickAudio = document.getElementById("clickAudio");
+
 const numContainer = document.querySelector(".num-cookies-container");
 const perSecContainer = document.querySelector(".per-second-container");
+const upgradeBtn = document.querySelectorAll(".upgrade-btn");
 
-const cursorBtn = document.querySelector(".cursor-btn");
 let perSec = 0;
+let cookies = 0;
+numContainer.textContent = "0";
+perSecContainer.textContent = "0";
 
 setInterval(() => {
   cookies += perSec / 10;
   numContainer.textContent = Math.floor(cookies);
 }, 100);
-
-const clickAudio = document.getElementById("clickAudio");
-
-let cookies = 0;
-numContainer.textContent = "0";
-perSecContainer.textContent = "0";
 
 cookie.addEventListener("click", () => {
   cookie.classList.remove("start-bouncing");
@@ -29,13 +28,17 @@ cookie.addEventListener("click", () => {
   numContainer.textContent = Math.floor(cookies);
 });
 
-cursorBtn.addEventListener("click", () => {
-  if (cookies >= 10) {
-    cookies -= 10;
-    numContainer.textContent = Math.floor(cookies);
+upgradeBtn.forEach((button) =>
+  button.addEventListener("click", () => {
+    const cost = parseInt(button.dataset.cost);
+    const rate = parseFloat(button.dataset.rate);
 
-    perSec += 0.1;
-    perSec = Math.round(perSec * 10) / 10;
-    perSecContainer.textContent = perSec;
-  }
-});
+    if (cookies >= cost) {
+      cookies -= cost;
+      perSec += rate;
+
+      numContainer.textContent = Math.floor(cookies);
+      perSecContainer.textContent = perSec.toFixed(1);
+    }
+  })
+);
